3fc524294c455f24fa30d3b049e67c6a51fbeef8
maggie <144709218+maggie-chang@users.noreply.github.com>
2024-10-21 18:56:58 +0000

    first text, scanner, while loop

--- YourAdventure.java
+++ YourAdventure.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+
 public class YourAdventure
 {
   public static void main(String[] args) 
   {
-    System.out.println("afkjhdfl");
+    Scanner scanner = new Scanner(System.in);
+    int silly = 100;
+
+    while (silly != 0) {
+      String firstText = "You cautiously strolled through the ominous dungeon while trying to find your cat companion.";
+      System.out.println(firstText); 
+      //String input1 = scanner.nextLine();
+
+
+    }
+
   }
 }
 
