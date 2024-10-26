e8e3e1906b12ff3578f576ff539a971eaf356564
maggie <144709218+maggie-chang@users.noreply.github.com>
2024-10-26 22:02:45 +0000

    finished opening text, outline for choice1input1 (COOKING SO HARD RN)

--- YourAdventure.java
+++ YourAdventure.java
@@ -5,16 +5,32 @@ public class YourAdventure
   public static void main(String[] args) 
   {
     Scanner scanner = new Scanner(System.in);
-    int silly = 100;
+    int sanity = 100;
 
-    while (silly != 0) {
-      String firstText = "You cautiously strolled through the ominous dungeon while trying to find your cat companion.";
-      System.out.println(firstText); 
-      //String input1 = scanner.nextLine();
-
+    while (sanity != 0) {
+      String openingText = "You cautiously meandered through the ominous dungeon while trying to find your cat companion. You can hear your rhythmic footsteps and the soft droplets falling from the overgrown vegetation. It was mostly quiet, but there was also something else--a distant tapping noise that seems to be nearing you. ";
+      
+      String choice1 = "\n (1) Run the opposite direction of the sound. \n (2) Approach the sound. \n (3) Hide. ";
+      
+      System.out.println(openingText + choice1);
+      String choice1Input1 = scanner.nextLine();
+      
+    if (choice1Input1.equals("1")) {
+          System.out.println("test1");
+    }
+    else if (choice1Input1.equals("2")) {
+          System.out.println("test2");
+    }
+    else if (choice1Input1.equals("3")) {
+        System.out.println("test3");
+    }
+    else {
+        System.out.println("Please enter options 1, 2, or 3");
+    }
+      
+    break; 
 
     }
 
   }
 }
-
---
+++ readme.md
@@ -0,0 +1,13 @@
+ideas:
+- mimic chest
+- freaky gojo (description)
+- "still water"
+- "red and blue"
+- "paper cup"
+- "im so sigma!"
+- "freddy fazbear"
+- "this must be the work of the dark reunion"
+- "cherry blossom ice tea"
+- "wait, they dont love me like i love you. WAIT"
+- "skibidi toilet"
+- 
